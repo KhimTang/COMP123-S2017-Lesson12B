ce50287bb7119ff65dc6c5832eb17a6dafed3cd4
Khim Tang <ktang33@my.centennialcollege.ca>
2017-08-02 22:28:15 -0400

    Created Demo Project

--- COMP123-S2017-Lesson12B/DemoForm.cs
+++ COMP123-S2017-Lesson12B/DemoForm.cs
@@ -8,11 +8,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+ * Name: Khim Tang
+ * StudentID: 300920672
+ * Date: August 02. 2017
+ * Description: Demo Application to showcase windows forms and UI Controls
+ * Version: 0.1 - Project was Created
+*/
+
 namespace COMP123_S2017_Lesson12B
 {
-    public partial class Form1 : Form
+    public partial class DemoForm : Form
     {
-        public Form1()
+        public DemoForm()
         {
             InitializeComponent();
         }
--- COMP123-S2017-Lesson12B/Program.cs
+++ COMP123-S2017-Lesson12B/Program.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+ * Name: Khim Tang
+ * StudentID: 300920672
+ * Date: August 02. 2017
+ * Description: Demo Application to showcase windows forms and UI Controls
+ * Version: 0.1 - Project was Created
+*/
+
 namespace COMP123_S2017_Lesson12B
 {
     static class Program
@@ -16,7 +24,7 @@ namespace COMP123_S2017_Lesson12B
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DemoForm());
         }
     }
 }
